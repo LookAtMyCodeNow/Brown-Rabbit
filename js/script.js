1ffc1365026ac84ac7decfb2c9f2437cfcb17c3e
LookAtMyCodeNow <paweljanuszewicz2022@gmail.com>
2024-03-06 18:58:05 +0100

    Create Info Section with pagination.

---
+++ js/script.js
@@ -0,0 +1,217 @@
+//  Function to rotate reading glass icon
+
+const glass = document.getElementById("rotate");
+
+glass.addEventListener("click", function () {
+  glass.classList.toggle("rotate");
+});
+
+// End Function to rotate reading glass icon
+
+// Data with images and text
+const data = [
+  {
+    imageUrl: `assets/images/posts/Post thumbnail-1.png`,
+    header: "Wonderful Copenhagen 2021",
+    textPublished: "Published 12/07/2021",
+    textInfo:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit Eligendi consectetur ea ipsam dolores! Laboriosam eveniet qui vel minus inventore id esse commodi unde quidem culpa a, quo dicta ad dolores deserunt quisquam necessitatibus,incidunt assumenda voluptatibus atque nobis earum odit...",
+  },
+  {
+    imageUrl: `assets/images/posts/Post thumbnail-2.png`,
+    header: "Wonderful Copenhagen 2021",
+    textPublished: "Published 12/07/2021",
+    textInfo:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit Eligendi consectetur ea ipsam dolores! Laboriosam eveniet qui vel minus inventore id esse commodi unde quidem culpa a, quo dicta ad dolores deserunt quisquam necessitatibus,incidunt assumenda voluptatibus atque nobis earum odit...",
+  },
+  {
+    imageUrl: `assets/images/posts/Post thumbnail-3.png`,
+    header: "Wonderful Copenhagen 2021",
+    textPublished: "Published 12/07/2021",
+    textInfo:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit Eligendi consectetur ea ipsam dolores! Laboriosam eveniet qui vel minus inventore id esse commodi unde quidem culpa a, quo dicta ad dolores deserunt quisquam necessitatibus,incidunt assumenda voluptatibus atque nobis earum odit...",
+  },
+  {
+    imageUrl: `assets/images/posts/Post thumbnail-4.png`,
+    header: "Wonderful Copenhagen 2021",
+    textPublished: "Published 12/07/2021",
+    textInfo:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit Eligendi consectetur ea ipsam dolores! Laboriosam eveniet qui vel minus inventore id esse commodi unde quidem culpa a, quo dicta ad dolores deserunt quisquam necessitatibus,incidunt assumenda voluptatibus atque nobis earum odit...",
+  },
+  {
+    imageUrl: `assets/images/posts/Post thumbnail-5.png`,
+    header: "Wonderful Copenhagen 2021",
+    textPublished: "Published 12/07/2021",
+    textInfo:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit Eligendi consectetur ea ipsam dolores! Laboriosam eveniet qui vel minus inventore id esse commodi unde quidem culpa a, quo dicta ad dolores deserunt quisquam necessitatibus,incidunt assumenda voluptatibus atque nobis earum odit...",
+  },
+  {
+    imageUrl: `assets/images/posts/Post thumbnail-6.png`,
+    header: "Wonderful Copenhagen 2021",
+    textPublished: "Published 12/07/2021",
+    textInfo:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit Eligendi consectetur ea ipsam dolores! Laboriosam eveniet qui vel minus inventore id esse commodi unde quidem culpa a, quo dicta ad dolores deserunt quisquam necessitatibus,incidunt assumenda voluptatibus atque nobis earum odit...",
+  },
+  {
+    imageUrl: `assets/images/posts/Post thumbnail-7.png`,
+    header: "Wonderful Copenhagen 2021",
+    textPublished: "Published 12/07/2021",
+    textInfo:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit Eligendi consectetur ea ipsam dolores! Laboriosam eveniet qui vel minus inventore id esse commodi unde quidem culpa a, quo dicta ad dolores deserunt quisquam necessitatibus,incidunt assumenda voluptatibus atque nobis earum odit...",
+  },
+  {
+    imageUrl: `assets/images/posts/Post thumbnail-8.png`,
+    header: "Wonderful Copenhagen 2021",
+    textPublished: "Published 12/07/2021",
+    textInfo:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit Eligendi consectetur ea ipsam dolores! Laboriosam eveniet qui vel minus inventore id esse commodi unde quidem culpa a, quo dicta ad dolores deserunt quisquam necessitatibus,incidunt assumenda voluptatibus atque nobis earum odit...",
+  },
+  {
+    imageUrl: `assets/images/posts/Post thumbnail-9.png`,
+    header: "Wonderful Copenhagen 2021",
+    textPublished: "Published 12/07/2021",
+    textInfo:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit Eligendi consectetur ea ipsam dolores! Laboriosam eveniet qui vel minus inventore id esse commodi unde quidem culpa a, quo dicta ad dolores deserunt quisquam necessitatibus,incidunt assumenda voluptatibus atque nobis earum odit...",
+  },
+  {
+    imageUrl: `assets/images/posts/Post thumbnail-10.png`,
+    header: "Wonderful Copenhagen 2021",
+    textPublished: "Published 12/07/2021",
+    textInfo:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit Eligendi consectetur ea ipsam dolores! Laboriosam eveniet qui vel minus inventore id esse commodi unde quidem culpa a, quo dicta ad dolores deserunt quisquam necessitatibus,incidunt assumenda voluptatibus atque nobis earum odit...",
+  },
+  {
+    imageUrl: `assets/images/posts/Post thumbnail-11.png`,
+    header: "Wonderful Copenhagen 2021",
+    textPublished: "Published 12/07/2021",
+    textInfo:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit Eligendi consectetur ea ipsam dolores! Laboriosam eveniet qui vel minus inventore id esse commodi unde quidem culpa a, quo dicta ad dolores deserunt quisquam necessitatibus,incidunt assumenda voluptatibus atque nobis earum odit...",
+  },
+  {
+    imageUrl: `assets/images/posts/Post thumbnail-12.png`,
+    header: "Wonderful Copenhagen 2021",
+    textPublished: "Published 12/07/2021",
+    textInfo:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit Eligendi consectetur ea ipsam dolores! Laboriosam eveniet qui vel minus inventore id esse commodi unde quidem culpa a, quo dicta ad dolores deserunt quisquam necessitatibus,incidunt assumenda voluptatibus atque nobis earum odit...",
+  },
+  {
+    imageUrl: `assets/images/posts/Post thumbnail-13.png`,
+    header: "Wonderful Copenhagen 2021",
+    textPublished: "Published 12/07/2021",
+    textInfo:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit Eligendi consectetur ea ipsam dolores! Laboriosam eveniet qui vel minus inventore id esse commodi unde quidem culpa a, quo dicta ad dolores deserunt quisquam necessitatibus,incidunt assumenda voluptatibus atque nobis earum odit...",
+  },
+  {
+    imageUrl: `assets/images/posts/Post thumbnail-14.png`,
+    header: "Wonderful Copenhagen 2021",
+    textPublished: "Published 12/07/2021",
+    textInfo:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit Eligendi consectetur ea ipsam dolores! Laboriosam eveniet qui vel minus inventore id esse commodi unde quidem culpa a, quo dicta ad dolores deserunt quisquam necessitatibus,incidunt assumenda voluptatibus atque nobis earum odit...",
+  },
+  {
+    imageUrl: `assets/images/posts/Post thumbnail-15.png`,
+    header: "Wonderful Copenhagen 2021",
+    textPublished: "Published 12/07/2021",
+    textInfo:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit Eligendi consectetur ea ipsam dolores! Laboriosam eveniet qui vel minus inventore id esse commodi unde quidem culpa a, quo dicta ad dolores deserunt quisquam necessitatibus,incidunt assumenda voluptatibus atque nobis earum odit...",
+  },
+];
+
+$(document).ready(function () {
+  // Items per page
+  const itemsPerPage = 3;
+
+  // Calculate total pages
+  const totalPages = Math.ceil(data.length / itemsPerPage);
+
+  // Initial page
+  let currentPage = 1;
+
+  // Function to display data for the current page
+  function displayData() {
+    const start = (currentPage - 1) * itemsPerPage;
+    const end = start + itemsPerPage;
+    const currentPageData = data.slice(start, end);
+
+    // Display data
+    $("#data-container").empty();
+    currentPageData.forEach((item) => {
+      const card = $("<div>").addClass("col p-0 mt-lg-3");
+      const cardMainContainer = $("<div>").addClass("card border-0");
+      const cardBody = $("<div>").addClass("row d-inline d-lg-flex");
+
+      const img = $(
+        `<img src="${item.imageUrl}" alt="Card Image" class="col-12 col-lg-4 ms-4 p-0 object-fit-cover img-fluid card-image">`
+      );
+
+      const cardFinalContainer = $("<div>").addClass(
+        "card-body col-lg-7 py-0 px-4 d-flex flex-column justify-content-between"
+      );
+      const header = $(`<h2>${item.header}</h2>`).addClass(
+        "card-title bolder-sm-screen mt-4 mt-lg-0"
+      );
+      const textPublised = $(`<p>${item.textPublished}</p>`).addClass(
+        "card-text text-warning h4"
+      );
+      const button = $(`<button type="button"
+      class="btn btn-lg btn-outline-dark rounded-0 align-self-baseline mt-2 mt-lg-0"
+      data-bs-toggle="modal"
+      data-bs-target="#articleModal">Read More</button>`);
+      const textInfo = $(`<p>${item.textInfo}</p>`).addClass("h5 lh-base");
+
+      card.append(cardMainContainer);
+      cardMainContainer.append(cardBody);
+      cardBody.append(img, cardFinalContainer);
+      cardFinalContainer.append(header, textPublised, textInfo, button);
+
+      $("#data-container").append(card);
+    });
+
+    // Display Page Description
+    $("#pageDescription")
+      .empty()
+      .append(`Page ${currentPage} of ${totalPages}`);
+
+    // Prev and Next Page Buttons
+    prevButton();
+    nextButton();
+  }
+
+  // Function to show prev page.
+  function prevButton() {
+    const prevButton = $("#prevButton");
+
+    prevButton.off("click");
+    prevButton.on("click", function () {
+      currentPage -= 1;
+      if (currentPage < 1) {
+        currentPage = totalPages;
+      }
+      console.log(currentPage);
+
+      setTimeout(() => {
+        displayData();
+      }, 200);
+    });
+  }
+
+  // Function to show next Page
+  function nextButton() {
+    const nextButton = $("#nextButton");
+
+    nextButton.off("click");
+    nextButton.on("click", function () {
+      currentPage += 1;
+      if (currentPage > totalPages) {
+        currentPage = 1;
+      }
+      console.log(currentPage);
+
+      setTimeout(() => {
+        displayData();
+      }, 200);
+    });
+  }
+
+  displayData();
+});
